93c4d90b8e60d30c6f0d43f3b49f46158cba69cc
IvannikovS <sergey03072002@mail.ru>
2025-05-26 18:35:04 +0300

    Add product-service

---
+++ product-service/src/main/java/com/ivannikov/microservices/product/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.ivannikov.microservices.product.controller;
+
+import com.ivannikov.microservices.product.dto.ProductRequest;
+import com.ivannikov.microservices.product.dto.ProductResponse;
+import com.ivannikov.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
